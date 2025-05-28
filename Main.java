927a045d757b19d86b209620d15feaaa91cd9178
rickyv07 <rickyvelchuri@gmail.com>
2025-05-28 08:40:19 -0700

    pet

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main{
     public static void main(String[] args){
 
-        System.out.println("hello hong");
+        System.out.println("Welcome to Talking Hong!");
 
 
-
-    }
+}
 }
