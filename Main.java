50f569e4f34446d152846c74effb0dc7fa5df74f
rickyv07 <rickyvelchuri@gmail.com>
2025-05-28 09:34:08 -0700

    pet & cat

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args){
 
-        System.out.println("Welcome to Talking Hong!");
+        System.out.println("Welcome to Pet Simulator!");
 
 
 }
--- Pet.java
+++ Pet.java
@@ -1,3 +1,33 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Pet {
-    
+    private String name;
+    private int weight;
+    private double food;
+
+    public Pet(String n, int w){
+        name = n;
+        weight = w;
+        food = 100.0;
+
+        Timer timer = new Timer();
+        
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                setFood();
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 2000, 4000);
+    }
+
+    public void feed(){
+        food = 100.0;
+    }
+
+    protected void setFood(){
+        food -= 1;
+    }
 }
