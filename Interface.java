9b94996fc23418520d0875586d5ff0bc3df25a23
agent965 <aiden.rothstein@gmail.com>
2025-05-29 13:50:34 -0700

    gui fixed + animal picture

--- Interface.java
+++ Interface.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,24 +17,18 @@ public class Interface extends JFrame implements ActionListener {
         setSize(400, 300);
         setLayout(new BorderLayout());
 
-        // Input Panel
-        JPanel inputPanel = new JPanel(new FlowLayout());
-        JLabel inputLabel = new JLabel("Enter text:");
-        inputField = new JTextField(20);
-        inputPanel.add(inputLabel);
-        inputPanel.add(inputField);
-
-        
 
         // Output Panel
         outputArea = new JTextArea(10, 30);
         JScrollPane scrollPane = new JScrollPane(outputArea);
         outputArea.setEditable(false);
         
-        JPanel gifPanel = new JPanel(new FlowLayout());
-        ImageIcon icon = new ImageIcon("OriginalNyan.gif"); 
-        JLabel imageLabel = new JLabel(icon);
-        gifPanel.add(imageLabel, BorderLayout.CENTER);
+        JPanel gifPanel = new JPanel();
+        ImageIcon icon = new ImageIcon("cat.jpg");
+        ;
+        JLabel gifLabel = new JLabel();
+        gifLabel.setIcon(icon);
+        gifPanel.add(gifLabel);
         
         // Button Panel
         JPanel buttonPanel = new JPanel(new FlowLayout());
@@ -40,20 +36,23 @@ public class Interface extends JFrame implements ActionListener {
         processButton.addActionListener(this);
         buttonPanel.add(processButton);
 
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        
+        centerPanel.add(gifLabel);
+        centerPanel.add(scrollPane);
+        
         // Add panels to frame
-        add(inputPanel, BorderLayout.NORTH);
-        add(scrollPane, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
-
+        add(centerPanel);
         setVisible(true);
     }
 
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == processButton) {
-            String inputText = inputField.getText();
-            outputArea.append(inputText + "\n");
-            inputField.setText(""); // Clear input field after processing
+            outputArea.append("fed!\n");
         }
     }
 }
