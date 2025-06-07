d818ba089756fa63cbb6f3b2671fd6d55d34fc7f
agent965 <aiden.rothstein@gmail.com>
2025-06-07 15:35:36 -0700

    .

--- Interface.java
+++ Interface.java
@@ -98,12 +98,14 @@ public class Interface extends JFrame implements ActionListener {
      petButton.addMouseListener(new java.awt.event.MouseAdapter() {
         @Override
         public void mousePressed(java.awt.event.MouseEvent e) {
-            imageLabel.setIcon(new ImageIcon("Assets/petMattHappy.gif"));
+            String mood = ((MattHong) currentPet).getState();
+            imageLabel.setIcon(new ImageIcon("Assets/pet"+mood+".gif"));
         }
     
         @Override
         public void mouseReleased(java.awt.event.MouseEvent e) {
-            imageLabel.setIcon(getScaledIcon("Assets/MattHappy.png", 100, 100));
+            String mood = ((MattHong) currentPet).getState();
+            imageLabel.setIcon(getScaledIcon("Assets/"+mood+".png", 100, 100));
         }
     });
      OnClickEventHelper.setOnClickColor(petButton, Color.decode("#7c6f97"), Color.decode("#bca8e4"));
@@ -204,12 +206,10 @@ public class Interface extends JFrame implements ActionListener {
             outputArea.append(currentPet.feed());
             System.out.println("Process");
         }
-        if(e.getSource() == petButton) {
-            imageLabel.setIcon(new ImageIcon("Assets/pet"+currentPet.getMood()+".gif"));
-            
-        }
+
         if(e.getSource() == pickHong){
             currentPet = new MattHong();
+            imageLabel.setIcon(getScaledIcon("Assets/MattHappy.png", 100, 100));
             petPanel.setVisible(true);
             startPanel.setVisible(false);
         }
